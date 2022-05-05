ddefab392b4434a5ba73aeeedd0a6b68899e54ab
Boonyarith Piriyothinkul <boonyarith.pir@gmail.com>
2022-05-05 17:52:53 +0700

    Show latest usage unit, update chart style

--- SmartWaterWeb/swmweb/src/component/chart/Chart.jsx
+++ SmartWaterWeb/swmweb/src/component/chart/Chart.jsx
@@ -7,6 +7,7 @@ import {
     Tooltip,
     Legend,
     ResponsiveContainer,
+    Label,
 } from "recharts";
 import { Card } from "@mui/material";
 import { useMemo } from "react";
@@ -21,12 +22,27 @@ export default function Chart({ title, data, dataKey, dateType }) {
         <Card sx={{ p: 1 }}>
             <h3 className="chartTitle">{title}</h3>
             <ResponsiveContainer width="100%" aspect={4 / 1}>
-                <BarChart data={interpolatedData}>
-                    <XAxis dataKey="name" stroke="#5550bd" />
-                    <YAxis stroke="#5550bd" />
+                <BarChart
+                    data={interpolatedData}
+                    margin={{ top: 15, right: 30, left: 20, bottom: 5 }}
+                >
+                    <XAxis dataKey="name" stroke="#5550bd">
+                        <Label value={dateType} position="insideBottomRight" />
+                    </XAxis>
+                    <YAxis stroke="#5550bd">
+                        <Label
+                            value="m^3"
+                            offset={-5}
+                            position="insideTopLeft"
+                        />
+                    </YAxis>
                     <Tooltip />
                     <Legend />
-                    <Bar dataKey={dataKey} fill="#8884d8" />
+                    <Bar
+                        dataKey={dataKey}
+                        fill="#8884d8"
+                        label={{ position: "top" }}
+                    />
                 </BarChart>
             </ResponsiveContainer>
         </Card>
--- SmartWaterWeb/swmweb/src/component/featuredInfo/FeaturedInfo.jsx
+++ SmartWaterWeb/swmweb/src/component/featuredInfo/FeaturedInfo.jsx
@@ -1,17 +1,17 @@
 import { Card } from "@mui/material";
 import { useEffect, useState } from "react";
-import { getWeeklyUsage } from "../../apis";
+import { getMonthlyUsage } from "../../apis";
 import "./featuredInfo.css";
 
 export default function FeaturedInfo() {
-    const [data, setData] = useState(null);
+    const [totalUsage, setTotalUsage] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         (async () => {
             try {
-                const resp = await getWeeklyUsage(new Date().getMonth() + 1);
-                setData(resp.data);
+                const resp = await getMonthlyUsage(new Date().getFullYear());
+                setTotalUsage(resp.data.at(-1).maxUsage);
             } catch (error) {
                 console.error(error);
             } finally {
@@ -22,8 +22,7 @@ export default function FeaturedInfo() {
 
     const renderContent = () => {
         if (isLoading) return "loading..."
-        if (!data.length) return 0
-        return data[data.length - 1].maxUsage
+        return totalUsage
     }
 
     return (
